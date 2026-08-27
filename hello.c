228ff040f3801af21d86033d44cf3981edc88bcc
Logan Yoder <lyoder3@huskers.unl.edu>
2026-08-27 12:47:25 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Logan Yoder
+ *         lyoder3@huskers.unl.edu
+ * Date: 2026/08/27
+ *
+ * A simple hack
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Logan Yoder\n");
+
+  return 0;
+}
